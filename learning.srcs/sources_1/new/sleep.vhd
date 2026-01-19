c915877ea06b2c554d52237be2c7750dd60cf609
Isaí García <50834416+isaigm@users.noreply.github.com>
2026-01-18 19:42:47 -0600

    Integrate counter component for LED control
    Added a counter component and activation signal to control LED increment.

--- learning.srcs/sources_1/new/sleep.vhd
+++ learning.srcs/sources_1/new/sleep.vhd
@@ -3,7 +3,6 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-
 entity sleep is
  Port ( 
         CLK100MHZ : in  std_logic; 
@@ -12,19 +11,23 @@ entity sleep is
 end sleep;
 
 architecture Behavioral of sleep is
-signal counter : unsigned(31 downto 0) := (others => '0');
+
+component counter is
+  Port (CLK100MHZ : in std_logic;
+        activate : out std_logic );
+end component;
+signal activate : std_logic := '0';
 signal number : unsigned(15 downto 0) := (others => '0');
+
 begin
+    cnt : counter port map(CLK100MHZ => CLK100MHZ, activate => activate);
     process (CLK100MHZ)
     begin 
         if rising_edge(CLK100MHZ) then
-            counter <= counter + 1;
-            if counter > 10_000_000 then
-                counter <= (others => '0');
+            if activate = '1' then
                 number <= number + 1;
             end if;
             LED <= std_logic_vector(number);
         end if;
     end process;
-
 end Behavioral;
