2d3759b22529cff93de52ff8ad6ca9b2499786fe
Isaí García <50834416+isaigm@users.noreply.github.com>
2026-01-20 13:33:58 -0600

    Parameterize LED width and add maxNumber function

--- learning.srcs/sources_1/new/sleep.vhd
+++ learning.srcs/sources_1/new/sleep.vhd
@@ -4,21 +4,27 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity sleep is
- Port ( 
+    generic(N: integer := 5); 
+    Port (
         CLK100MHZ : in  std_logic; 
-        LED       : out std_logic_vector (15 downto 0)
-    );
+        LED       : out std_logic_vector (N - 1 downto 0)
+        );
 end sleep;
 
 architecture Behavioral of sleep is
-
+function maxNumber (nbits : integer) return integer is
+    variable i : integer;
+    variable max : unsigned(N downto 0) := to_unsigned(1, N + 1);
+    begin
+        return to_integer(shift_left(max, N) - 1);
+end function;
 component counter is
   Port (CLK100MHZ : in std_logic;
         activate : out std_logic );
 end component;
 signal activate : std_logic := '0';
-signal number : unsigned(15 downto 0) := (others => '0');
-
+signal number : unsigned(N - 1 downto 0) := (others => '0');
+signal max: unsigned (N - 1 downto 0) := to_unsigned(maxNumber(N), N);
 begin
     cnt : counter port map(CLK100MHZ => CLK100MHZ, activate => activate);
     process (CLK100MHZ)
@@ -26,6 +32,9 @@ begin
         if rising_edge(CLK100MHZ) then
             if activate = '1' then
                 number <= number + 1;
+            end if;
+            if number > max then
+                number <= (others => '0');
             end if;
             LED <= std_logic_vector(number);
         end if;
